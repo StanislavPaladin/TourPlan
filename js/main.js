f7b85ce26810947f6fad555a584bca9919408a8e
StanislavPaladin <Paladinstanis@gmail.com>
2020-07-28 20:39:49 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ var hotelSlider = new Swiper('.hotel-slider', {
         onlyInViewport: false,
       },
   });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function() {
+  console.log("клик по кнопке меню");
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
+});
