2503b45066263680096178ecda1c83731c76ff62
StanislavPaladin <Paladinstanis@gmail.com>
2020-08-08 13:51:43 +0300

    fixed bugs

---
+++ js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+  });
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+  });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function() {
+  console.log("клик по кнопке меню");
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
+});
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close")
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+
+function openModal() {
+  var targetModal = $(this).attr("data-href");
+  $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+  $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
+
+//обработка форм
+$(".form").each(function () {
+  $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "The name must be at least 2 characters long",
+      },
+      email: {
+        required: "TEST",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "Phone is required",
+      },
+    },
+  });
+});
+AOS.init();
+});
